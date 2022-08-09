950f0b0ea4c159ac388bde6e493fe0364667c501
Nasam Sayour <nasamsay@gmail.com>
2022-08-09 17:19:52 +0300

    commit tree ca441574eb7c6cfce2599a992295af61939ac04f

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code your solution here
+function saturdayFun(arg ='roller-skate'){
+    return `This Saturday, I want to ${arg}!` ;
+}
+
+console.log(saturdayFun('reading'))
+
+function mondayWork(arg='go to the office'){
+    return `This Monday, I will ${arg}.`;
+}
+
+mondayWork('code')
+
+
+function wrapAdjective(visual='*'){
+    return function (adj='special'){
+        return `You are ${visual}${adj}${visual}!`
+    }
+}
+
+console.log(wrapAdjective('%')('a dedicated programmer'))
